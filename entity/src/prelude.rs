c71f52496cb1adae6fcf3697da649957dd222c9f
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-06-14 10:16:13 -0300

    Delete entity directory

--- entity/src/prelude.rs
+++
@@ -1,6 +0,0 @@
-pub use super::access_token::Entity as AccessToken;
-pub use super::kv_store::Entity as KvStore;
-pub use super::macaroon::Entity as Macaroon;
-pub use super::node::Entity as Node;
-pub use super::payment::Entity as Payment;
-pub use super::seaql_migrations::Entity as SeaqlMigrations;
