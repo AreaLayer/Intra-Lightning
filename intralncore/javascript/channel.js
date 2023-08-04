30e396ee8c7c8192711b3ef7abf68699bca981a8
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-04 06:49:00 -0300

    Update channel.js

--- intralncore/javascript/channel.js
+++ intralncore/javascript/channel.js
@@ -1,1 +1,13 @@
+import * as rustModule from 'rust-lightning';
+
+// Initialize LDK
+rustLightning.init();
+
+// Open a channel
+const fundingAmount = 100000; // Replace with desired amount
+const channelInfo = rustLightning.openChannel(fundingAmount);
+
+// Close a channel
+const channelId = channelInfo.id; // Replace with actual channel ID
+rustLightning.closeChannel(channelId);
 
