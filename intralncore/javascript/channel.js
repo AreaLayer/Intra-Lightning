c950ca5f79dc2abe0d0c4513bc67906637802457
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-04 07:04:34 -0300

    Update channel.js

--- intralncore/javascript/channel.js
+++ intralncore/javascript/channel.js
@@ -7,6 +7,10 @@ rustLightning.init();
 const fundingAmount = 100000; // Replace with desired amount
 const channelInfo = rustLightning.openChannel(fundingAmount);
 
+// Rebalce a channel
+const reblanceAmount = 10000;
+const ChannelInfo = rustLightning.reblanceChannel(rebalanceAmount);
+
 // Close a channel
 const channelId = channelInfo.id; // Replace with actual channel ID
 rustLightning.closeChannel(channelId);
