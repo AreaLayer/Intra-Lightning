48d78c1480359477656a2dac36a4b85a9df2233f
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-15 07:41:20 -0300

    Update tor.js

--- intralncore/javascript/tor.js
+++ intralncore/javascript/tor.js
@@ -1,1 +1,18 @@
+import WebSocket from 'ws';
+import { SocksProxyAgent } from 'socks-proxy-agent';
 
+const agent = new SocksProxyAgent(
+	'socks://your-name%40gmail.com:abcdef12345124@br41.nordvpn.com'
+);
+
+var socket = new WebSocket('ws://echo.websocket.events', { agent });
+
+socket.on('open', function () {
+	console.log('"open" event!');
+	socket.send('hello world');
+});
+
+socket.on('message', function (data, flags) {
+	console.log('"message" event! %j %j', data, flags);
+	socket.close();
+});
